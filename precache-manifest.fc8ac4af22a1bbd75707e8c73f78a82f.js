8e4a1da40656fff6c34f02d6fec68a14e001da37
Pushkaraj1992 <pushkaraj.kawade@gmail.com>
2020-09-22 17:33:57 +0530

    Updates

---
+++ precache-manifest.fc8ac4af22a1bbd75707e8c73f78a82f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27097ab0a4a63963893297538d126bcf",
+    "url": "/Quotes/index.html"
+  },
+  {
+    "revision": "357f03561107e4bc7bd2",
+    "url": "/Quotes/static/css/main.b1edc8f4.chunk.css"
+  },
+  {
+    "revision": "01b93beb14a9726196ee",
+    "url": "/Quotes/static/js/2.a9bd7ef3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quotes/static/js/2.a9bd7ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357f03561107e4bc7bd2",
+    "url": "/Quotes/static/js/main.fa0895a4.chunk.js"
+  },
+  {
+    "revision": "5e3ecca330f8bb32b223",
+    "url": "/Quotes/static/js/runtime-main.5b7d123c.js"
+  }
+]);
